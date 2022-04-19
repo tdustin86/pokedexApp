3c4b9f72f62de7f8018d5828418c142e558a05b3
Dustin Turner <101357412+tdustin86@users.noreply.github.com>
2022-04-19 16:23:35 +0200

    changed how I formatted the content in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
-let pokemonList = [
-{name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying']}
-{name: 'Beedrill', height: 1, weight: 29.5, type: ['bug', 'poison']}
-{name: 'Bellsprout', height: 0.7, weight: 4, type: ['grass', 'poison']}
-{name: 'Dewgong', height: 1.7, weight: 120, type: ['ice', 'water']}
-{name: 'Zapdos', height: 1.6, weight: 52.6, type: ['electric', 'flying']}
-{name: 'Xatu', height: 1.5, weight: 15, type: ['psychic', 'flying']}
-  
+let pokemonList = [{
+    name: 'Charizard',
+    height: 1.7,
+    weight: 90.5,
+    type: ['fire', 'flying']
+  } {
+    name: 'Beedrill',
+    height: 1,
+    weight: 29.5,
+    type: ['bug', 'poison']
+  } {
+    name: 'Bellsprout',
+    height: 0.7,
+    weight: 4,
+    type: ['grass', 'poison']
+  } {
+    name: 'Dewgong',
+    height: 1.7,
+    weight: 120,
+    type: ['ice', 'water']
+  } {
+    name: 'Zapdos',
+    height: 1.6,
+    weight: 52.6,
+    type: ['electric', 'flying']
+  } {
+    name: 'Xatu',
+    height: 1.5,
+    weight: 15,
+    type: ['psychic', 'flying']
+  }
+
 ]
